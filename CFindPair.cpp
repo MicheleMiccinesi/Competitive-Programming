deb70d8135a20123cb7cd191d716f9f9d4098f02
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-09-30 16:48:55 +0200

    sync_with_stdio(false)

--- CFindPair.cpp
+++ CFindPair.cpp
@@ -2,11 +2,15 @@
 /* lexycographic order reduces to divide and modulo operations... */
 /* time cost: O(nlogn) */
 /* space cost: O(n) */
+/* would it be faster on an array of N_MAX=10000 elements  */
+/* considering that we don't need to initialize its elements? */
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 int main(){
+ std::ios_base::sync_with_stdio(false);
+ 
  std::vector<int64_t> numeri;
  int64_t k,n,q,r,temp;
  std::cin >> n >> k;
