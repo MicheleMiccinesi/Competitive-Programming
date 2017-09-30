a693856b1efb21c4b071b2366dd24480da79779e
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-09-30 21:48:54 +0200

    C(ready for testing) Towers

--- CTowers.cpp
+++ CTowers.cpp
@@ -1,10 +1,17 @@
+/* Towers */
+/* Time cost: O(n), n: number of bars */
+/* Space cost: O(N), N: possible heights */
+/* With hashing we would save on space, lose on time */
+/* With N=1000 the array should stay in L1 or L2 cache */
 #include <iostream>
 #include <array>
+
+const int N = 1000;
 
 int main(){
  std::ios_base::sync_with_stdio(false);
 
- std::array<int,1001> towers;
+ std::array<int,N+1> towers;
  towers.fill(0);
 
  int n;
@@ -12,7 +19,7 @@ int main(){
 
  int temp;
  int max=1;
- for(;n>0;n--){
+ while( n-- ){
   std::cin >> temp;
   ++towers[temp];
   if( towers[temp] == 1 )
