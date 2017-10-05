ef41dc702481016d08aa4dff575465daa045ef69
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-10-05 15:57:44 +0200

    Update CFindingTeamMember.cpp

--- CFindingTeamMember.cpp
+++ CFindingTeamMember.cpp
@@ -1,9 +1,3 @@
-/* Finding Team Member */
-/* Before writing down this solution, which is essentially an experimentation in C++, */
-/* I was lingering with another one in which I defined a structure in which each row */
-/* has an order given by strenght of the couple, while on columns I was keeping the order */
-/* given by index... trying to use array, since n_max is only 400...  */
-
 #include <iostream>
 #include <list>
 #include <vector>
@@ -13,19 +7,19 @@ struct t_couple {
  t_couple(unsigned int ind1, unsigned int ind2, unsigned int stg) 
  : strenght {stg}
  {
-  if(ind1<ind2) {inds[0]= ind1;inds[1]=ind2;}
+  if(ind1<ind2) {inds[0]=ind1;inds[1]=ind2;}
   else {inds[0]=ind2; inds[1]=ind1;};
  };
- friend bool operator<(const t_couple& l,const t_couple& r){
-  return l.strenght<r.strenght;
+ friend bool const operator<(const t_couple& l,const t_couple& r){
+  return l.strenght < r.strenght;
  };
- friend bool operator>(const t_couple& l,const t_couple& r){
-  return l.strenght>r.strenght;
+ friend bool const operator>(const t_couple& l,const t_couple& r){
+  return l.strenght > r.strenght;
  };
- friend bool operator==(const t_couple& l,const t_couple& r){
+ friend bool const operator==(const t_couple& l,const t_couple& r){
   return l.inds[0]==r.inds[0] || l.inds[1]==r.inds[1] || l.inds[0]==r.inds[1] || l.inds[1]==r.inds[0]; 
  };
- const unsigned int& operator[] (const unsigned int& i){
+ unsigned int const& operator[] (const unsigned int& i){
   return inds[i%2];
  }; 
  ~t_couple(){};
@@ -35,24 +29,20 @@ private:
 };
 
 std::vector<unsigned int>& find_couples(std::list<t_couple>& lista, std::vector<unsigned int>& result){
- if(!lista.empty()){
-  t_couple& temp=lista.back();
-  while(!lista.empty()){
-   temp=lista.back();
-   result[temp[0]-1]=temp[1];
-   result[temp[1]-1]=temp[0];
-   lista.remove(temp);
-  }
+ while(!lista.empty()){
+  result[lista.back()[0]-1]=lista.back()[1];
+  result[lista.back()[1]-1]=lista.back()[0];
+  lista.remove(lista.back());
  }
  return result;
 }
 
-std::list<t_couple>& read_strenght(unsigned int n, std::list<t_couple>& lista){
+std::list<t_couple>& read_strenght(unsigned int& n, std::list<t_couple>& lista){
  unsigned int temp;
- for(auto i=1;i!=n+1;++i)
-  for(auto j=1;j!=i;++j){
+ for(unsigned int i=1;i!=n+1;++i)
+  for(unsigned int j=1;j!=i;++j){
    std::cin >> temp;
-   lista.push_back(t_couple(i,j,temp));
+   lista.emplace_back(i,j,temp);
   };
  lista.sort();
  return lista; 
@@ -60,7 +50,7 @@ std::list<t_couple>& read_strenght(unsigned int n, std::list<t_couple>& lista){
 
 int main(){
  std::ios_base::sync_with_stdio(false);
- 
+
  unsigned int n;
  std::cin >> n;
  n*=2;
@@ -69,6 +59,6 @@ int main(){
  std::list<t_couple> strenght;
  read_strenght(n,strenght);
  find_couples(strenght,result);
- for(auto i=0;i<n;++i) std::cout << result[i] << " ";
+ for(unsigned int i=0;i!=n;++i) std::cout << result[i] << " ";
  std::cout << std::endl;
 }
