e5f19d4662449f9a0caba0372052403195baba2c
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-10-06 02:35:58 +0200

    Create CInversionCount.cpp

---
+++ CInversionCount.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+
+unsigned int N_MAX=200000;
+
+template <typename T> 
+struct vecmer{
+ vecmer (unsigned int& n) :
+  base{vettore1}, temp{vettore2}, inversions{0}
+ {
+  vettore1.reserve(n); vettore1.resize(n);
+  vettore2.reserve(n); vettore2.resize(n);
+ }
+ void resize (unsigned int& n) 
+ {
+  vettore1.resize(n);
+  vettore2.resize(n);
+ }
+ auto& operator[] (int i) {return base[i];};
+ void merge_sort(){
+  inversions=0;
+  ti=base.begin();
+  tf=base.end();
+  couple_sort();
+  tf=temp.begin()+base.size();
+  int block=2;
+  while(base.size()>block){
+   ti=temp.begin();
+   li=base.begin();
+   lf=ri=li+block;
+   rf=ri+block;
+   if(rf>base.end())rf=base.end();
+   do {
+    merge();
+    if(base.end()-ri <= block)
+     while(ri != base.end()) *ti++ = *ri++;
+    else {
+     li=rf;
+     lf=ri=li+block;
+     rf=ri+block;
+     if(rf>base.end())rf=base.end();
+    }
+   }
+    while(ti!=tf);
+   block<<=1;
+   swap();
+  }
+ }
+ uint64_t const& inv(){ return inversions; }
+ std::vector<T>& base;
+ std::vector<T>& temp;
+ ~vecmer(){};
+private:
+ uint64_t inversions;
+ std::vector<T> vettore1;
+ std::vector<T> vettore2; 
+ typename std::vector<T>::iterator li,lf,ri,rf,ti,tf;
+ void merge(){
+  while(li!=lf && ri!=rf){
+   if(*ri < *li){
+    inversions += lf-li;
+    *ti++ = *ri++;
+   }
+   else *ti++ = *li++;
+  }
+  if(li==lf) 
+   while(ri!=rf) *ti++ = *ri++;
+  else 
+   while(li!=lf) *ti++ = *li++;
+ }
+ void couple_sort(){
+  T Tt;
+  while(ti < tf-1){
+   if(*ti > *(ti+1)){
+    Tt=*ti; 
+    *ti=*(ti+1);
+    *(ti+1)=Tt;
+    ++inversions;
+   }
+   ti+=2;
+  }
+ }
+ void swap(){
+  std::vector<T>& tmp=base;
+  base=temp;
+  temp=tmp;
+ }
+};
+
+int main(){
+ std::ios_base::sync_with_stdio(false);
+ vecmer<int> miovettore(N_MAX);
+ unsigned int T,n; int i,val;
+ std::cin >> T;
+ while(T--){
+  std::cin >> n;
+  miovettore.resize(n);
+  for(i=0;i<n;i++) std::cin >> miovettore[i];
+  miovettore.merge_sort();
+  std::cout << miovettore.inv() << std::endl;
+ }
+}
