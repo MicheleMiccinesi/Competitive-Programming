1cf1315278bef94e2562977e3f98451366297d26
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-10-08 22:00:38 +0200

    Update CLargestEvenNumber.cpp

--- CLargestEvenNumber.cpp
+++ CLargestEvenNumber.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <string>
 
 int main(){
  std::ios_base::sync_with_stdio(false);
  unsigned int T,c,temp;
- char d;
+ std::string number;
  std::cin >> T;
  int numeri[11];
+
  while(T--){
   numeri[10]=10; for(temp=0;temp!=10;temp++)numeri[temp]=0;
-  do std::cin.get(d); while(d<48 || d>57);
-  while(d>47 && d<58){
-   c=d-48;
+  std::cin >> number;
+
+  for(auto& x: number){
+   c=x-'0';
    ++numeri[c];
-   if(c%2 == 0 && c<numeri[10])numeri[10]=c;
-   std::cin.get(d);
+   if(c%2 == 0 && c<numeri[10]) numeri[10]=c;
   }
+
   temp=10;
   if(numeri[10]==10)
    while(temp--)
@@ -26,5 +29,7 @@ int main(){
    std::cout << numeri[10];
   }
   std::cout << std::endl;
+  number.clear();
  }
+ 
 }
