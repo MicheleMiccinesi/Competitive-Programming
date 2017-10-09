6402e853b412810f109b3dec77e53704643a8d3e
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-10-09 22:26:49 +0200

    Update CInversionCount.cpp

--- CInversionCount.cpp
+++ CInversionCount.cpp
@@ -18,31 +18,28 @@ struct vecmer{
  }
  auto& operator[] (int i) {return base[i];};
  void merge_sort(){
-  inversions=0;
-  ti=base.begin();
-  tf=base.end();
-  couple_sort();
-  tf=temp.begin()+base.size();
-  int block=2;
-  while(base.size()>block){
-   ti=temp.begin();
-   li=base.begin();
-   lf=ri=li+block;
-   rf=ri+block;
-   if(rf>base.end())rf=base.end();
+  inversions = 0;
+  tf = temp.begin() + base.size();
+  block = 1;
+  while(base.size() > block){
+   ti = temp.begin();
+   li = base.begin();
+   lf = ri = li + block;
+   rf = ri + block;
+   if(rf > base.end()) rf = base.end();
    do {
     merge();
-    if(base.end()-ri <= block)
+    if(base.end() - ri <= block)
      while(ri != base.end()) *ti++ = *ri++;
     else {
-     li=rf;
-     lf=ri=li+block;
-     rf=ri+block;
-     if(rf>base.end())rf=base.end();
+     li = rf;
+     lf = ri = li + block;
+     rf = ri + block;
+     if(rf > base.end()) rf = base.end();
     }
    }
-    while(ti!=tf);
-   block<<=1;
+    while(ti != tf);
+   block <<= 1;
    swap();
   }
  }
@@ -51,34 +48,23 @@ struct vecmer{
  std::vector<T>& temp;
  ~vecmer(){};
 private:
+ int block;
  uint64_t inversions;
  std::vector<T> vettore1;
  std::vector<T> vettore2; 
  typename std::vector<T>::iterator li,lf,ri,rf,ti,tf;
  void merge(){
-  while(li!=lf && ri!=rf){
+  while(li != lf && ri != rf){
    if(*ri < *li){
     inversions += lf-li;
     *ti++ = *ri++;
    }
    else *ti++ = *li++;
   }
-  if(li==lf) 
-   while(ri!=rf) *ti++ = *ri++;
+  if(li == lf) 
+   while(ri != rf) *ti++ = *ri++;
   else 
-   while(li!=lf) *ti++ = *li++;
- }
- void couple_sort(){
-  T Tt;
-  while(ti < tf-1){
-   if(*ti > *(ti+1)){
-    Tt=*ti; 
-    *ti=*(ti+1);
-    *(ti+1)=Tt;
-    ++inversions;
-   }
-   ti+=2;
-  }
+   while(li != lf) *ti++ = *li++;
  }
  void swap(){
   std::vector<T>& tmp=base;
@@ -89,13 +75,14 @@ private:
 
 int main(){
  std::ios_base::sync_with_stdio(false);
- vecmer<int> miovettore(N_MAX);
- unsigned int T,n; int i,val;
+
+ vecmer<int> miovettore( N_MAX );
+ unsigned int T,n; int i;
  std::cin >> T;
- while(T--){
+ while( T-- ){
   std::cin >> n;
   miovettore.resize(n);
-  for(i=0;i<n;i++) std::cin >> miovettore[i];
+  for(i=0; i<n; i++) std::cin >> miovettore[i];
   miovettore.merge_sort();
   std::cout << miovettore.inv() << std::endl;
  }
