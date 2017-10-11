3d4c5450ca55b2fbc57dd608622c3977c38b1c2c
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-10-11 23:00:41 +0200

    Create MaxPathSum.cpp

---
+++ MaxPathSum.cpp
@@ -0,0 +1,16 @@
+int maxPathSum_(struct Node *root, int& max){
+    if(!root) return 0;
+    int maxL = maxPathSum_(root->left, max);
+    int maxR = maxPathSum_(root->right, max);
+    if(maxR + root->data + maxL > max) max = maxL + root->data + maxR;
+    if(maxR > maxL) return maxR + root->data;
+    return maxL + root->data;
+}
+
+int maxPathSum(struct Node *root)
+{
+ static int max_path_sum;
+ max_path_sum = 0;
+ maxPathSum_(root, max_path_sum);
+ return max_path_sum;
+}
