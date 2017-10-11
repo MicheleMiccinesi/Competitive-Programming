066a895885933ed7fb4ba248b0fc8bb4b34cde7e
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-10-11 22:15:28 +0200

    Create CheckBST.cpp

---
+++ CheckBST.cpp
@@ -0,0 +1,14 @@
+#include <limits>
+bool isBST_(Node *root, int& last_seen) {
+    if( !root ) return true;
+    if( !isBST_( root->left, last_seen ) ) return false;
+    if( root->data < last_seen ) return false;
+    last_seen = root->data;
+    return isBST_( root->right, last_seen );
+}
+
+bool isBST(Node* root){
+    static int last_seen;
+    last_seen = std::numeric_limits<int>::min();
+    return isBST_(root, last_seen);    
+}
