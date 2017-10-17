61403c4434972ac8109916dfe89bbfe936ccb386
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-10-17 18:13:58 +0200

    Create LittleGirlMaxSum.cpp

---
+++ LittleGirlMaxSum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+ std::ios_base::sync_with_stdio( false );
+
+ int32_t n;
+ std::cin >> n;
+ int32_t q;
+ std::cin >> q;
+
+ std::vector<uint32_t> numeri(n+2);
+ for( auto i = numeri.begin() + 1; i != numeri.end() - 1; ++i ) std::cin >> *i;
+
+ std::vector<int32_t> queries(n+2,0);
+ uint32_t query;
+ while( q-- ){
+  std::cin >> query;
+  ++queries[query];
+  std::cin >> query;
+  --queries[query+1];
+ }
+
+ for( auto x = queries.begin()+2; x != queries.end()-1; x++) *x += *(x-1);
+
+ std::sort(queries.begin()+1, queries.end()-1);
+ std::sort(numeri.begin()+1, numeri.end()-1);
+ uint64_t maxSum{0};
+ int64_t lll{1};
+ do maxSum += lll*numeri[n]*queries[n]*lll;
+  while(--n);
+ 
+ std::cout << maxSum << std::endl;
+}
