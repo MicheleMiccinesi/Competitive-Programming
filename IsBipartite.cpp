a95e5c966ca199326c1c66d0ba066a6223e9c1c3
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2017-11-02 18:09:14 +0100

    Update IsBipartite.cpp

--- IsBipartite.cpp
+++ IsBipartite.cpp
@@ -19,16 +19,7 @@ bool isBipartite(int G[][MAX],int V)
         if( !marked[i] ){
             groupMark[i]=0;
             marked[i]=1;
-            for(j=0;j!=V;j++) if(G[i][j]) {
-                if(marked[j]){ 
-                    if(groupMark[i]==groupMark[j]) return false; 
-                }
-                else {
-                    groupMark[j]=!groupMark[i];
-                    marked[j]=1;
-                    visiting.push(j);
-                }
-            }
+            visiting.push(i);
             while( !visiting.empty() ){
                 k=visiting.front();
                 for(j=0;j!=V;j++) if(G[k][j]) {
