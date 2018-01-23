bae884b6c8c5498a0674c46ca383384cf9aefdaa
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2018-01-23 14:10:06 +0100

    Create PythonIndentation.cpp

---
+++ more/PythonIndentation.cpp
@@ -0,0 +1,61 @@
+/* Python Indentation */
+/* Problem Source: Python Indentation on codeforces */
+/* Python Indentation */
+/* License: contact the author */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+const int mod = 1000000007;
+
+inline void get_blocks(std::vector<int>& b, const int &n){
+	std::string c;
+	for( int i=0, j=0; i!=n; ++i ){
+		c.clear();
+		while(c.empty())
+			std::getline(std::cin, c);
+		switch( c[0] ){
+			case 's':
+				b.push_back(j);
+				j=0;
+				break;
+			case 'f':
+				++j;
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+
+	int n;
+	std::cin >> n;
+
+	//blocks of code with delta indendation
+	std::vector<int> b;												
+	b.reserve(n);
+	get_blocks(b, n);
+	int i = b.size()-1;
+
+	int s=1;
+	for( int j=0; j!=i; ++j )
+		s+=b[j];
+
+	std::vector<int> S(s,1);
+	int B=0;
+	while(i--){
+		for( int j=B+1; j<S.size(); ++j ){
+			S[j]+=S[j-1];
+			if(S[j]>mod) S[j]-=mod;
+		}
+		B+=b[i];
+	}
+	std::cout << S.back() << std::endl;
+
+	return 0;
+}
