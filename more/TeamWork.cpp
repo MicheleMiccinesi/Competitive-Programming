a7d351c5d93ae99a809dabfb7e096a4128c5e2c2
MicheleMiccinesi <32134789+MicheleMiccinesi@users.noreply.github.com>
2018-02-15 20:19:58 +0100

    Update TeamWork.cpp

--- more/TeamWork.cpp
+++ more/TeamWork.cpp
@@ -7,20 +7,15 @@
 
 const uint64_t mod=1000000007;
 
-void f(std::vector<uint64_t>& T, int& k, int& i, const int& N){
-	if(k==0)
-		return;
-	{
-		int ii=i+1;
-		while( i ){
-			(T[i]*=i)+=(N-i+1)*T[i-1];
-			T[i]%=mod;
-			--i;
+//k>0, N>1
+inline void f(std::vector<uint64_t>& T, const int& k, const int& N){
+	for( int i=1; i!=k+1; ++i ){
+		for( int j=i, n=N-i+1; j; --j, ++n ){
+			(T[j]*=j)+=n*T[j-1];
+			T[j]%=mod;
 		}
 		T[0]=0;
-		i=ii;
 	}
-	f(T, --k, i, N);
 }
 
 int main(){
@@ -35,10 +30,7 @@ int main(){
 
 	std::vector<uint64_t> T(k+1, 0);
 	T[0]=1;
-	{
-		int kk=k, ii=1;
-		f(T, kk, ii, N);
-	}
+	f(T, k, N);
 
 	std::vector<uint64_t> p2(k+1,0);
 	{
@@ -63,7 +55,7 @@ int main(){
 		p2[i]=(p2[i-1]*500000004)%mod;
 
 	uint64_t res=0;
-	for( int i=0; i!=k+1; ++i ){
+	for( int i=0; i!=k+1&&i!=N+1; ++i ){
 		res+=(p2[i]*T[i]);
 		res%=mod;
 	}
